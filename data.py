179989629a91a285df824a922d194113e4331fc6
Kyialbeginner <kiyal.sku@gmail.com>
2021-10-04 00:36:25 -0600

    Create Auth.app

---
+++ data.py
@@ -0,0 +1,18 @@
+USERS =[
+    {
+        "fullnames": "Alex Zolo",
+        "username": "zulo123",
+        "password": "super_pass2021",
+        "logged_in": False
+    },
+    {   "fullnames": "AJ Meyani",
+        "username": "aj-cool",
+        "password": "aj-boy2021",
+        "logged_in": False
+    },
+    {   "fullnames": "Shanon Ruden",
+        "username": "shanon-lady",
+        "password": "shaly2021",
+        "logged_in": False
+    }
+]
